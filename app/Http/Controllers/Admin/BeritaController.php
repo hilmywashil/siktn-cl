072b19a4334c2251c90cb8a6452f72ff77162e58
Naooo <naooolaf@gmail.com>
2026-08-03 23:55:17 +0700

    feat: implement BeritaController for admin content management and regional approval workflows

--- app/Http/Controllers/Admin/BeritaController.php
+++ app/Http/Controllers/Admin/BeritaController.php
@@ -163,6 +163,7 @@ class BeritaController extends Controller
         } catch (\Throwable $e) {
             $kategorisDb = [];
         }
+        $kategoris = array_values(array_unique(array_merge(['Pengumuman', 'Kegiatan', 'Regulasi'], $kategorisDb)));
         $daftarProvinsi = \App\Helpers\WilayahHelper::getDaftarProvinsi();
 
         return view('admin.berita.edit', compact('admin', 'berita', 'kategoris', 'daftarProvinsi'));
